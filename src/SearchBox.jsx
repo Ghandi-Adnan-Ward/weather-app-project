05cd1764f057c25b28fa7ee9ec2df574b070ab78
ghandi ward <ghandi.ward81@gmail.com>
2024-09-08 13:53:20 +0300

    http

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -2,59 +2,68 @@
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
+import axios from 'axios';
 
-
-function SearchBox({updateInfo}) { 
+function SearchBox({ updateInfo }) {
     let [city, setCity] = useState('');
     const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-    const API_kEY = "7db2108be8f9b821bda8b70ca32b7754";
-
+    const API_KEY = "7db2108be8f9b821bda8b70ca32b7754";
 
     let getWeatherInfo = async () => {
-        let response = await fetch(`${API_URL}?q=${city}&appid=${API_kEY}&units=matric`);
-        let jsonResponse = await response.json();
-        console.log(jsonResponse);
+        try {
+            let response = await axios.get(`${API_URL}`, {
+                params: {
+                    q: city,
+                    appid: API_KEY,
+                    units: 'metric'
+                }
+            });
+            let res = response.data;
+            console.log(res);
 
-        let result = {
-          city :city,
-            temp : jsonResponse.main.temp,
-            tempMin : jsonResponse.main.temp_min,
-            tempMax : jsonResponse.main.temp_max,
-            humidity : jsonResponse.main.humidity,
-            feelsLike : jsonResponse.main.feels_like,
-            weather : jsonResponse.weather[0].main,
+            let result = {
+                city: city,
+                temp: res.main.temp,
+                tempMin: res.main.temp_min,
+                tempMax: res.main.temp_max,
+                humidity: res.main.humidity,
+                feelsLike: res.main.feels_like,
+                weather: res.weather[0].main,
+            };
 
+            console.log(result);
+            return result;
+        } catch (error) {
+            console.error("Error fetching the weather data: ", error);
         }
+    };
 
-        console.log(result);
-        return result;
-    }
-    
+    let handleChange = (event) => {
+        setCity(event.target.value);
+    };
 
+    let handleSubmit = async (event) => {
+        event.preventDefault();
+        console.log(city);
+        setCity("");
+        let newInfo = await getWeatherInfo();
+        if (newInfo) {
+            updateInfo(newInfo);
+        }
+    };
 
-let handleChange = (event) => {
-    setCity(event.target.value);
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <TextField id="city" label="City Name" variant="outlined" required value={city} onChange={handleChange} />
+                <br></br>
+                <br></br>
+                <Button variant="contained" type="submit">
+                    Search
+                </Button>
+            </form>
+        </div>
+    );
 }
 
-let handleSubmit = async (event) => {
-    event.preventDefault();
-    console.log(city);
-    setCity("");
-    let newInfo = await getWeatherInfo();
-    updateInfo(newInfo); 
-}
-  return (
-    <div>
-    <form onSubmit={handleSubmit}>
-    <TextField id="city" label="City Name" variant="outlined" required value={city} onChange={handleChange}/>
-    <br></br>
-    <br></br>
-    <Button variant="contained" type="submit">
-        Search
-      </Button>
-    </form>
-    </div>
-  )
-}
-
-export default SearchBox
+export default SearchBox;
