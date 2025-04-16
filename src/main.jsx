7ccd3488f1885ea9366a4600d0fd7cce440010b5
ghandi ward <ghandi.ward81@gmail.com>
2025-04-16 15:21:59 +0300

    upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { HashRouter,    } from "react-router-dom";
-
+import { BrowserRouter as Router,   } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <HashRouter basename="/weather-app">
-          <App/>
-      </HashRouter>  
+   <Router basename="/weather-app-project">
+      <App/>
+    </Router>  
   </React.StrictMode>,
 )
